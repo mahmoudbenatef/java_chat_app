ae77283be8cc1afae5f8c38a0f6bbee1cd1b06e4
leenasherif239 <sherifleena@yahoo.com>
2021-02-22 21:42:58 +0200

    add_playerclass_

--- SocketServerChat/src/socketserverchat/Classes/Player.java
+++ SocketServerChat/src/socketserverchat/Classes/Player.java
@@ -10,5 +10,69 @@ package socketserverchat.Classes;
  * @author atef
  */
 public class Player {
+     private int id;
+    private String username;
+    private String password;
+    private int points;
+    private int flag;
+    private String nickName = "";
+
+    public Player(String username, String password ) {
+        this.username = username;
+        this.password = password;
+        this.nickName = nickName;
+
+    }
+    public Player()
+    {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getFlag() {
+        return flag;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
 
 }

0ec612142501d84f137fc7314421a769d53104c6
Maymaher <mai.maher003@gmail.com>
2021-02-22 22:34:35 +0200

    chatroom my message class

--- TicTacToe/src/chatroom/Classes/MyMessage.java
+++ TicTacToe/src/chatroom/Classes/MyMessage.java
@@ -10,5 +10,27 @@ package chatroom.Classes;
  * @author atef
  */
 public class MyMessage {
-    
+    private String username;
+    private String message;
+
+    public MyMessage(String username, String message) {
+        this.username = username;
+        this.message = message;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }

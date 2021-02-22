da05144e71e7acb9af5d8d622861bfcf2f7dac16
hosam94 <houssamali94@gmail.com>
2021-02-22 23:04:03 +0200

    add chatRoom methods

--- TicTacToe/src/chatroom/ChatRoom.java
+++ TicTacToe/src/chatroom/ChatRoom.java
@@ -514,20 +514,492 @@ public class ChatRoom extends Application {
 
         return gridPane;
     }   
-}
+public String getUsername() {
+        return username;
+    }
 
+    public GridPane mainPage() {
+        // properties of text 
+        GridPane grid = new GridPane();
+        grid.setVgap(10);
+        grid.setHgap(10);
+        grid.setPadding(new Insets(2, 2, 2, 2));
+        grid.setId("second-pane");
+
+        // option list 
+        playerComboBox = new ComboBox();
+
+        playerComboBox = new ComboBox();
+        for (Player pp : playerList) {
+            playerComboBox.getItems().add(
+                    pp.getUsername() + "," + pp.getPoints() + "," + pp.getFlag()
+            );
+        }
+
+        playerComboBox.setValue("please choose one player");
+
+        // Set the CellFactory property
+        playerComboBox.setCellFactory(new ShapeCellFactory());
+        // Set the ButtonCell property
+        playerComboBox.setButtonCell(new ShapeCell());
+        ////////////////////////////////////////////////
+        playerComboBox.setValue("please choose one player");
+        playerComboBox.setId(("combo-box"));
+
+        // handle of option list 
+        playerComboBox.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (playerComboBox.getSelectionModel().getSelectedIndex() > -1) {
+
+                    int index = playerComboBox.getSelectionModel().getSelectedIndex();
+                    username = playerList.get(index).getUsername();
+                    userActiveFlag = playerList.get(index).getFlag();
+
+                }
+            }
+
+        });
+
+        Label headerLabel = new Label("Welcome To The Game");
+         scoreLabel = new Label("Score " + myScore);
+        headerLabel.setFont(Font.font("Verdana", FontPosture.ITALIC, 20));
+        scoreLabel.setFont(Font.font("Verdana", FontPosture.ITALIC, 1));
+        GridPane.setHalignment(headerLabel, HPos.CENTER);
+        headerLabel.setId("main-title");
+        scoreLabel.setId("submain-title");
+
+        // radio button
+        ToggleGroup radioGroup = new ToggleGroup();
+        RadioButton radioButton1 = new RadioButton("computer");
+        radioButton1.setId("radio-button");
+        RadioButton radioButton2 = new RadioButton("player");
+        radioButton1.setToggleGroup(radioGroup);
+        radioButton2.setToggleGroup(radioGroup);
+        radioButton2.setId("radio-button");
+
+        radioGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> ov, Toggle t, Toggle t1) {
+
+                RadioButton chk = (RadioButton) t1.getToggleGroup().getSelectedToggle(); // Cast object to radio button
+                if (chk.getText().equals("computer")) {
+                    playWithBot = true;
+                }
+
+            }
+        });
+
+        VBox hbox = new VBox(radioButton1, radioButton2);
+        hbox.setSpacing(10);
+        //play button
+        playButton = new Button("play");
+        playButton.setPrefHeight(70);
+        playButton.setDefaultButton(true);
+        playButton.setPrefWidth(130);
+        playButton.setId("buttons");
+
+        playButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                if (playWithBot) {
+                    resumeGame = true;
+                    ps.println("resume play");
+
+                    isX = true;
+                } else if (userActiveFlag == 1) {
+                    ps.println("chat");
+                    ps.println(username);
+                    isX = true;
+                } else {
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            Alert alertActive = new Alert(AlertType.ERROR);
+                            alertActive.setTitle("Error Message");
+                            alertActive.setHeaderText("You can't play with offline player");
+                            alertActive.showAndWait();
+                        }
+                    });
+                }
+
+            }
+        });
+
+        alertLabel = new Label("alert");
+        alertLabel.setVisible(false);
+
+        grid.add(headerLabel, 2, 2);
+        grid.add(scoreLabel, 2, 4);
+        grid.add(playerComboBox, 2, 10);
+        grid.add(hbox, 2, 8);
+        grid.add(playButton, 10, 22);
+        grid.add(alertLabel, 10, 52);
+
+        return grid;
+
+    }
+
+    public boolean isPlayWithBot() {
+        return playWithBot;
+    }
+
+    public BorderPane playPage(String user) {
+        textMessageArea = new TextArea();
+        textMessageArea.setEditable(false);
+        textMessageArea.setPrefSize(300, 345);
+        AnchorPane chatPane = new AnchorPane();
+        BorderPane.setAlignment(chatPane, Pos.CENTER);
+        chatPane.setPrefSize(277, 400);
+
+        textMessageArea.setLayoutX(5);
+        textMessageArea.setLayoutY(5);
+        if (!playWithBot) {
+
+            chatPane.getChildren().add(textMessageArea);
+        }
+
+        TextField textField = new TextField();
+
+        textField.setPrefWidth(180.0);
+
+        Button sendButton = new Button("send");
+        sendButton.setPrefWidth(50);
+        sendButton.setId("buttons2");
+        backToMenuButton = new Button("back");
+        backToMenuButton.setPrefWidth(50);
+        backToMenuButton.setId("buttons2");
+
+        sendButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                MyMessage myMessage = new MyMessage(user, textField.getText());
+                ps.println("send message");
+                ps.println(new Gson().toJson(myMessage));
+                textField.clear();
+            }
+        });
+
+        backToMenuButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                if (playWithBot == false) {
+                    ps.println("back");;
+                } else {
+                    resumeGame = false;
+                    playWithBot = false;
+                    turn = "o";
+                    isX = false;
+                    ps.println("save map");
+                    Gson gson = new Gson();
+                    String myMap = gson.toJson(cellValues(), String[][].class);
+                    ps.println(myMap);
+                    map = null;
+                    backToMenuButton.getScene().setRoot(mainPage());
+
+                }
+
+            }
+        });
+
+        Insets insets = new Insets(20);
+        HBox hBox;
+        if (!playWithBot) {
+            hBox = new HBox(10, textField, sendButton, backToMenuButton);
+        } else {
+            hBox = new HBox(10, backToMenuButton);
+        }
+        BorderPane root = new BorderPane();
+        if (!playWithBot) {
+
+            root.setCenter(textMessageArea);
+        }
+        root.setId("left-borderPane");
+        BorderPane.setMargin(textMessageArea, insets);
+        root.setBottom(hBox);
+        BorderPane.setMargin(hBox, insets);
+        borderPane = new BorderPane();
+        borderPane.setId("second-pane");
+        gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                Cell cell = new Cell(this);
+                gridPane.add(cell, j, i);
+                board[i][j] = cell;
+            }
+        }
+        if (resumeGame) {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    if (map != null) {
+                        board[i][j].getPlayerMove().setText(map[i][j]);
+                    }
+                }
+
+            }
+        }
+        borderPane.setCenter(gridPane);
+        borderPane.setRight(root);
+
+        return borderPane;
+
+    }
+
+    public GridPane requestPage(String user) {
+
+        GridPane gp = new GridPane();
+        gp.setAlignment(Pos.CENTER);
+        gp.setHgap(10);
+        gp.setVgap(10);
+        gp.setPadding(new Insets(25, 25, 25, 25));
+        gp.setId("second-pane");
+
+        Label notation = new Label(user + " wants to play with you, do you accept?");
+        notation.setId("main-title");
+
+        Button acceptButton = new Button("accept");
+        Button cancelButton = new Button("cancel");
+
+        acceptButton.setId("acceptbtn");
+        cancelButton.setId("acceptbtn");
+
+        acceptButton.setPrefHeight(40);
+        acceptButton.setPrefWidth(90);
+
+        cancelButton.setPrefHeight(40);
+        cancelButton.setPrefWidth(90);
+        acceptButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                acceptButton.getScene().setRoot(playPage(user));
+                ps.println("accepted");
+                ps.println(user);
+            }
+        });
+
+        cancelButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                cancelButton.getScene().setRoot(mainPage());
+            }
+        });
+
+        GridPane.setHalignment(notation, HPos.CENTER);
+        GridPane.setHalignment(acceptButton, HPos.CENTER);
+        GridPane.setHalignment(cancelButton, HPos.CENTER);
+        gp.add(notation, 0, 0);
+        HBox requestHBox = new HBox(10.0, acceptButton, cancelButton);
+        requestHBox.setAlignment(Pos.CENTER);
+        GridPane.setHalignment(requestHBox, HPos.CENTER);
+        gp.add(requestHBox, 0, 1);
+
+        return gp;
+    }
+
+    public void winMessage(String whoWon) {
+        String msg;
+        if (!whoWon.equals("x")) {
+            msg = "O won!";
+        } else {
+            msg = "X won!";
+        }
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                Alert gameEndAlert = new Alert(Alert.AlertType.INFORMATION);
+                gameEndAlert.setTitle("game ended");
+                gameEndAlert.setHeaderText(msg);
+                gameEndAlert.showAndWait();
+                turn = "o";
+                isX = false;
+                backToMenuButton.getScene().setRoot(mainPage());
+
+            }
+        });
+
+    }
+
+    public void cleanMap() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j].getPlayerMove().setText("");
+            }
+        }
+
+    }
+
+    public void drawMessage() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                Alert gameEndAlert = new Alert(Alert.AlertType.INFORMATION);
+                gameEndAlert.setTitle("game ended");
+                gameEndAlert.setHeaderText("draw!");
+                gameEndAlert.showAndWait();
+                turn = "o";
+                isX = false;
+                backToMenuButton.getScene().setRoot(mainPage());
+
+            }
+
+        });
+    }
+
+    public String[][] cellValues() {
+        String[][] values = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                values[i][j] = board[i][j].getPlayerMove().getText().toString();
+            }
+        }
+        return values;
+    }
+
+    public DataInputStream getDis() {
+        return dis;
+    }
+
+    public PrintStream getPs() {
+        return ps;
+    }
+
+    public boolean isX() {
+        return isX;
+    }
+
+    public String getTurn() {
+        return turn;
+    }
+
+    public String getMyUserName() {
+        return myUserName;
+    }
+
+    public boolean playerWon() {
+        //row
+        boolean won = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][0].getPlayerMove().getText().equals(board[i][1].getPlayerMove().getText())
+                    && board[i][0].getPlayerMove().getText().equals(board[i][2].getPlayerMove().getText())
+                    && !board[i][0].getPlayerMove().getText().isEmpty()) {
+                won = true;
+                winner = board[i][0].getPlayerMove().getText();
+            }
+        }
+        //column
+        for (int i = 0; i < board.length; i++) {
+            if (board[0][i].getPlayerMove().getText().equals(board[1][i].getPlayerMove().getText())
+                    && board[0][i].getPlayerMove().getText().equals(board[2][i].getPlayerMove().getText())
+                    && !board[0][i].getPlayerMove().getText().isEmpty()) {
+                won = true;
+                winner = board[0][i].getPlayerMove().getText();
+            }
+        }
+        //diagonal
+        if (board[0][0].getPlayerMove().getText().equals(board[1][1].getPlayerMove().getText())
+                && board[0][0].getPlayerMove().getText().equals(board[2][2].getPlayerMove().getText()) && !board[0][0].getPlayerMove().getText().isEmpty()) {
+            won = true;
+            winner = board[0][0].getPlayerMove().getText();
+        }
+        if (board[0][2].getPlayerMove().getText().equals(board[1][1].getPlayerMove().getText())
+                && board[0][2].getPlayerMove().getText().equals(board[2][0].getPlayerMove().getText()) && !board[0][2].getPlayerMove().getText().isEmpty()) {
+            won = true;
+            winner = board[0][2].getPlayerMove().getText();
+        }
+
+        return won;
+    }
+
+    public void winMessage() {
+        String msg;
+        if (winner.equals("o")) {
+            msg = "O won!";
+        } else {
+            msg = "X won!";
+        }
+
+        Alert gameEndAlert = new Alert(Alert.AlertType.INFORMATION);
+        gameEndAlert.setTitle("game ended");
+        gameEndAlert.setHeaderText(msg);
+        gameEndAlert.showAndWait();
+        // ChatRoom
+    }
+
+    public void botMove() {
+
+        while (true) {
+            int x = new Random().nextInt(2 - 0 + 1) + 0;
+            int y = new Random().nextInt(2 - 0 + 1) + 0;
+            if (board[x][y].getPlayerMove().getText().isEmpty()) {
+                board[x][y].getPlayerMove().setText("o");
+                break;
+            }
+        }
+
+    }
+
+    public boolean gameOver() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j].getPlayerMove().getText().isEmpty()) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+}
 //To edit combobox shape
 
 class ShapeCell extends ListCell<String> {
+
+    @Override
+    public void updateItem(String item, boolean empty) {
+        super.updateItem(item, empty);
+
+        if (empty) {
+            setText(null);
+            setGraphic(null);
+        } else {
+            setText("");
+            GridPane cellShape = this.getShape(item);
+            setGraphic(cellShape);
+        }
+    }
+
+    public GridPane getShape(String shapeType) {
+        GridPane shape = null;
+        Circle status;
+
+        if (shapeType != null) {
+            String[] inputItemsSplit = shapeType.split(",");
+            if (Integer.valueOf(inputItemsSplit[2]) == 1) {
+                status = new Circle(3, Color.GREEN);
+            } else {
+                status = new Circle(3, Color.RED);
+            }
+            Label playerNameStatus = new Label(inputItemsSplit[0], status);
+            Label playerScore = new Label("Score: " + inputItemsSplit[1]);
+            shape = new GridPane();
+            shape.add(playerNameStatus, 0, 0);
+            shape.add(playerScore, 0, 1);
+        }
+
+        return shape;
+    }
 }
 
 class ShapeCellFactory implements Callback<ListView<String>, ListCell<String>> {
 
     @Override
-    public ListCell<String> call(ListView<String> param) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ListCell<String> call(ListView<String> listview) {
+        return new ShapeCell();
     }
-
 }
-
-
